a14bba3fe475cbc50873ad9ac2cf9d5238e62517
Julie Campbell <julie@mindcapers.com>
2025-05-05 01:33:15 -0400

    First commit

---
+++ wintones.py
@@ -0,0 +1,157 @@
+# wintones: python notes player
+# Copyright (c) 2025 Mindcapers LLC
+# MIT License
+
+import winsound
+
+# Note values rounded from https://doctormix.com/blog/note-to-frequency-chart/  
+# Some notes will be more off than others, but winsound only takes integers, but
+# good enough for computer beeping
+
+# NOTE_A0 = 27
+# NOTE_AS0 = 29
+# NOTE_BF0 = 29
+# NOTE_B0 = 31
+# NOTE_C1 = 33
+# NOTE_CS1 = 35
+# NOTE_DF1 =35
+
+# winsound: ValueError: frequency must be in 37 thru 32767
+NOTE_D1 = 37
+NOTE_DS1 = 39
+NOTE_EF1 = 39
+NOTE_E1 = 41
+NOTE_F1 = 44
+NOTE_FS1 = 46
+NOTE_GF1 = 46
+NOTE_G1 = 49
+NOTE_GS1 = 52
+NOTE_AF1 = 52
+NOTE_A1 = 55
+NOTE_AS1 = 58
+NOTE_BF1 = 58
+NOTE_B1 = 62
+NOTE_C2 = 65
+NOTE_CS2 = 69
+NOTE_DF2 = 69
+NOTE_D2 = 72
+NOTE_DS2 = 78
+NOTE_EF2 = 78
+NOTE_E2 = 82
+NOTE_F2 = 87
+NOTE_FS2 = 92
+NOTE_GF2 = 92
+NOTE_G2 = 98
+NOTE_GS2 = 104
+NOTE_AF2 = 104
+NOTE_A2 = 110
+NOTE_AS2 = 117
+NOTE_BF2 = 117
+NOTE_B2 = 124
+
+# C3 was the lowest note I could hear through my speakers
+NOTE_C3 = 131
+NOTE_CS3 = 139
+NOTE_DF3 = 139
+NOTE_D3 = 147
+NOTE_DS3 = 156
+NOTE_EF3 = 156
+NOTE_E3 = 165
+NOTE_F3 = 174
+NOTE_FS3 = 185
+NOTE_GF3 = 185
+NOTE_G3 = 196
+NOTE_GS3 = 208
+NOTE_AF3 = 208
+NOTE_A3 = 220
+NOTE_AS3 = 233
+NOTE_BF3 = 233
+NOTE_B3 = 247
+NOTE_C4 = 261
+NOTE_CS4 = 277
+NOTE_DF4 =277
+NOTE_D4 = 297
+NOTE_DS4 = 311
+NOTE_EF4 = 311
+NOTE_E4 = 330
+NOTE_F4 = 349
+NOTE_FS4 = 370
+NOTE_GF4 = 370
+NOTE_G4 = 392
+NOTE_GS4 = 415
+NOTE_AF4 = 415
+NOTE_A4 = 440
+NOTE_AS4 = 466
+NOTE_BF4 = 466
+NOTE_B4 = 494
+NOTE_C5 = 523
+NOTE_CS5 = 554
+NOTE_DF5 =554
+NOTE_D5 = 587
+NOTE_DS5 = 622
+NOTE_EF5 = 622
+NOTE_E5 = 659
+NOTE_F5 = 698
+NOTE_FS5 = 740
+NOTE_GF5 = 740
+NOTE_G5 = 783
+NOTE_GS5 = 830
+NOTE_AF5 = 830
+NOTE_A5 = 880
+NOTE_AS5 = 932
+NOTE_BF5 = 932
+NOTE_B5 = 987
+NOTE_C6 = 1046
+NOTE_CS6 = 1109
+NOTE_DF6 = 1109
+NOTE_D6 = 1175
+NOTE_DS6 = 1245
+NOTE_EF6 = 1245
+NOTE_E6 = 1319
+NOTE_F6 = 1397
+NOTE_FS6 = 1480
+NOTE_GF6 = 1480
+NOTE_G6 = 1568
+NOTE_GS6 = 1661
+NOTE_AF6 = 1661
+NOTE_A6 = 1760
+NOTE_AS6 = 1865
+NOTE_BF6 = 1865
+NOTE_B6 = 1976
+NOTE_C7 = 2093
+NOTE_CS7 = 2217
+NOTE_DF7 = 2217
+NOTE_D7 = 2349
+NOTE_DS7 = 2489
+NOTE_EF7 = 2489
+NOTE_E7 = 2637
+NOTE_F7 = 2794
+NOTE_FS7 = 2960
+NOTE_GF7 = 2960
+NOTE_G7 = 3136
+NOTE_GS7 = 3322
+NOTE_AF7 = 3322
+NOTE_A7 =  3520
+NOTE_AS7 = 3729
+NOTE_BF7 = 3729
+NOTE_B7 = 3951
+NOTE_C8 = 4186
+
+def GetNoteList():
+    localNoteList = [  
+        NOTE_C3,    NOTE_DF3,   NOTE_D3,    NOTE_EF3,   NOTE_E3,    NOTE_F3,
+        NOTE_GF3,   NOTE_G3,    NOTE_AF3,   NOTE_A3,    NOTE_BF3,   NOTE_B3,
+        NOTE_C4,    NOTE_DF4,   NOTE_D4,    NOTE_EF4,   NOTE_E4,    NOTE_F4,
+        NOTE_GF4,   NOTE_G4,    NOTE_AF4,   NOTE_A4,    NOTE_BF4,   NOTE_B4,
+        NOTE_C5,    NOTE_DF5,   NOTE_D5,    NOTE_EF5,   NOTE_E5,    NOTE_F5,
+        NOTE_GF5,   NOTE_G5,    NOTE_AF5,   NOTE_A5,    NOTE_BF5,   NOTE_B5,
+        NOTE_C6,    NOTE_DF6,   NOTE_D6,    NOTE_EF6,   NOTE_E6,    NOTE_F6,
+        NOTE_GF6,   NOTE_G6,    NOTE_AF6,   NOTE_A6,    NOTE_BF6,   NOTE_B6,
+        NOTE_C7,    NOTE_DF7,   NOTE_D7,    NOTE_EF7,   NOTE_E7,    NOTE_F7,
+        NOTE_GF7,   NOTE_G7,    NOTE_AF7,   NOTE_A7,    NOTE_BF7,   NOTE_B7,
+        NOTE_C8
+    ]
+    
+    return localNoteList
+
+
---
+++ wintones_samples.py
@@ -0,0 +1,36 @@
+# wintones: python notes player
+# Copyright (c) 2025 Mindcapers LLC
+# MIT License
+
+# https://www.geeksforgeeks.org/python-winsound-module/
+
+import winsound
+import wintones
+import time
+
+def playAll(noteList, len):
+    for note in noteList:
+        winsound.Beep(int(note), len)
+        time.sleep((len/1000.0) + 0.1)
+
+def scaleF3(len):
+    winsound.Beep(int(wintones.NOTE_F3), len)
+    winsound.Beep(int(wintones.NOTE_G3), len)
+    winsound.Beep(int(wintones.NOTE_A3), len)
+    winsound.Beep(int(wintones.NOTE_BF3), len)
+    winsound.Beep(int(wintones.NOTE_C4), len)
+    winsound.Beep(int(wintones.NOTE_D4), len)
+    winsound.Beep(int(wintones.NOTE_E4), len)
+    winsound.Beep(int(wintones.NOTE_F4), len)
+
+
+winsound.Beep(int(wintones.NOTE_C4), 500);
+time.sleep(1)
+
+notes = wintones.GetNoteList()
+playAll(notes, 200)
+time.sleep(1)
+
+scaleF3(200)
+
+
